9f80b4c2cc58b76624b589be79b2393881bf3458
claire froelich <claire.froelich@gmail.com>
2020-01-14 16:46:48 -0500

    permit new user parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :set_title
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+       def configure_permitted_parameters
+           # devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :city, :email, :password) }
+           # devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
+           devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :city])
+       end
 
   private
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,6 +42,12 @@ class UsersController < ApplicationController
     redirect_to my_account_path
   end
 
+  def account_create
+    @user = current_user
+    @user.update(user_params)
+    redirect_to my_account_path
+  end
+
   private
 
   def user_params
