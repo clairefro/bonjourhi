5b44f83e9a744d0174bf9f365a7391f1e674e4e9
claire froelich <claire.froelich@gmail.com>
2020-01-15 19:49:59 -0500

    handle speechSynth timeout error

--- app/javascript/packs/plugins/speechSynthesis.js
+++ app/javascript/packs/plugins/speechSynthesis.js
@@ -1,22 +1,34 @@
+// unclog speechSynthesis if still stuck speaker after x milliseconds
+const timeoutSpeech = (wait) => {
+  setTimeout(() => {
+    if (speechSynthesis.speaking){
+      window.speechSynthesis.cancel();
+      console.log('speech timed out. Canceled.')
+    }
+  }, wait);
+}
+
+const speakWithTimeout = (utterance) => {
+  window.speechSynthesis.speak(utterance)
+  timeoutSpeech(8000);
+}
+
 const speakText = (text, lang) => {
   let u = new SpeechSynthesisUtterance();
-
   u.text = text;
   u.lang = lang;
-
-  window.speechSynthesis.speak(u);
+  speakWithTimeout(u);
+  console.log('yooooo')
 };
 
 const listenForSpeakerClick = () => {
    const vocabItemList = document.querySelectorAll('.vocab-item-list');
    vocabItemList.forEach((vocabGroup) => {
      vocabGroup.addEventListener('click', (e) => {
-
      // only fire if speaker icon (<i>) was clicked
       if(e.target.nodeName === "I") {
         const text = e.target.dataset.text;
         const lang = e.target.dataset.isoCode;
-        console.log(text);
         speakText(text,lang);
       }
     });
